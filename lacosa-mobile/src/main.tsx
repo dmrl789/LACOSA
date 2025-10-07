32aa070d84301a7cdfdb474012c03af5651b497b
dmrl789 <ippan.org@gmail.com>
2025-10-07 17:40:24 +0200

    Scaffold mobile client and add dev tooling

--- lacosa-mobile/src/main.tsx
+++ lacosa-mobile/src/main.tsx
@@ -1,16 +1,6 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App'
 
-const rootElement = document.getElementById("root");
-
-if (!rootElement) {
-  throw new Error("Root element not found");
-}
-
-createRoot(rootElement).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+const root = createRoot(document.getElementById('root')!)
+root.render(<App />)
